129329ae58db83ae9afc3ac75eac6d59b9d48162
ary <ary@clappingape.com>
2018-12-16 07:43:43 +0700

    add coconut structure

--- app/src/main/java/app/beelabs/fingol/IConfig.kt
+++ app/src/main/java/app/beelabs/fingol/IConfig.kt
@@ -1,4 +1,13 @@
 package app.beelabs.fingol
 
 interface IConfig {
+    val API_BASE_URL: String get() = ""
+    val DEFAULT_LOADING: String get() = ""
+
+    // key api
+
+    // session key
+
+    // result key
+
 }
